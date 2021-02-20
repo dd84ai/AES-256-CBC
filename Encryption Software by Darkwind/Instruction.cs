d4dd59b76717b2fd19471507cde25dc29b724647
dd84ai <dark.dreamflyer2@gmail.com>
2021-02-20 14:42:12 +0700

    fix: cleaned from everything, renamed and prepared for deploy

--- Encryption Software by Darkwind/Instruction.cs
+++ Encryption Software by Darkwind/Instruction.cs
@@ -15,7 +15,10 @@ namespace Encryption_Software_by_Darkwind
         public Instruction()
         {
             InitializeComponent();
-            richTextBox1.Text = "Encrypt: Encrypt will allow you to create codes of contract for your friends to try decode without the registered key code.\n\nDecrypt: Decrypt will allow you to able decode a Encrypted code and will show what was written when a Generated code was met from when it was Encrypted\n\nGenerated Key: Self explainer, It just Generates a random made code to give to friends for easy decrypted messsages.\n\nP.S. To cut it short, when you encrypt text, it's important which key you use, as it would be needed to decode it back.\n\nWhen you want to decode a message - you need correct encrypted code and a correct key to it, to get a readable decrypted text.";
+            richTextBox1.Text = "Encrypt: This button encodes your text, which could not be decoded without the key\n\n" +
+                "Decrypt: This button decodes encrypted text back to readable. It requires having the right key\n\n" + "" +
+                "Generate Key: This button exists for the cases when you are lazy to create your key on your own\n\n" + 
+                "P.S. To cut it short, when you encrypt text, it's important which key you use, as it would be needed to decode it back.\n\nWhen you want to decode a message - you need correct encrypted text and a correct key to it in order to get a readable decrypted text back.";
         }
     }
 }
